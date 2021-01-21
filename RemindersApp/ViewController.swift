207a6e30cdabc1ffcbaaf30eb7339353deab7259
Lumkooo <shaminandrey2013@gmail.com>
2021-01-21 12:01:58 +0700

    Initial commit

---
+++ RemindersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemindersApp
+//
+//  Created by Андрей Шамин on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
